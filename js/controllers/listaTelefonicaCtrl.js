a34c2b05381a3a9be050aec7d2d7d49bff6d968c
David <david.dmr@gmail.com>
2016-08-25 10:25:46 -0300

    Criação de serviço service para url de getOperadoras request

--- js/controllers/listaTelefonicaCtrl.js
+++ js/controllers/listaTelefonicaCtrl.js
@@ -1,5 +1,5 @@
 //Definindo o controller
-angular.module("listaTelefonica").controller("listaTelefonicaCtrl", function ($scope, $http, contatosAPI){
+angular.module("listaTelefonica").controller("listaTelefonicaCtrl", function ($scope, $http, contatosAPI, operadorasAPI){
     $scope.app = "Lista Telefonica";
     /*$scope.contatos = [
         {nome: "Pedro", telefone: "9999-9999", data: new Date(), operadora: {nome: "Oi", codigo: 14, categoria: "Celular"} ,cor: "blue"},
@@ -23,7 +23,7 @@ angular.module("listaTelefonica").controller("listaTelefonicaCtrl", function ($s
     };
 
     var carregarOperadoras = function () {
-    	$http.get("http://localhost:3412/operadoras").success(function (data, status) {
+    	operadorasAPI.getOperadoras().success(function (data, status) {
     		console.log(data);
     		$scope.operadoras = data;
     	});
