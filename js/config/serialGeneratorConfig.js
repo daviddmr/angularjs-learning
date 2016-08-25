735b203deeaf3bcf0465f9b5afba6cd271ed309f
David <david.dmr@gmail.com>
2016-08-25 15:02:33 -0300

    Implementação de um config para o provider serial Generator

---
+++ js/config/serialGeneratorConfig.js
@@ -0,0 +1,6 @@
+/**
+ * Created by davidmelo on 8/25/16.
+ */
+angular.module("listaTelefonica").config(function (serialGeneratorProvider) {
+  serialGeneratorProvider.setLenght(5);
+});
--- js/services/serialGeneratorService.js
+++ js/services/serialGeneratorService.js
@@ -2,11 +2,21 @@
  * Created by davidmelo on 8/25/16.
  */
 angular.module("listaTelefonica").provider("serialGenerator", function () {
+  var _length = 20;
+
+  this.setLenght = function (length) {
+    _length = length;
+  };
+
+  this.getLenght = function () {
+    return _length;
+  };
+
   this.$get = function () {
     return {
       generate: function () {
         var serial = "";
-        while (serial.length < 10) {
+        while (serial.length < _length) {
           serial += String.fromCharCode(Math.floor(Math.random() * 64) + 32);
         }
         return serial;
