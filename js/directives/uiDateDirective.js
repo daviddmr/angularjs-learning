1f0ab2d267578268c21a92a841936cf1ee356b21
David <david.dmr@gmail.com>
2016-08-26 18:23:54 -0300

    Usando a diretiva para transformar o valor da data

--- js/directives/uiDateDirective.js
+++ js/directives/uiDateDirective.js
@@ -5,8 +5,22 @@ angular.module("listaTelefonica").directive("uiDate", function () {
   return {
     require: 'ngModel',
     link: function (scope, element, attrs, ctrl) {
+      var _formatDate = function (date) {
+        date = date.replace(/[^0-9]+/g, "");
+        if(date.length > 2) {
+          date = date.substring(0,2) + "/" + date.substring(2);
+        }
+        if(date.length > 5) {
+          date  = date.substring(0,5) + "/" + date.substring(5,9);
+        }
+        return date;
+      };
+
       element.bind("keyup", function () {
-        console.log(ctrl.$viewValue);
+        //Setando o valor do viewValue com uma concatenação
+        ctrl.$setViewValue(_formatDate(ctrl.$viewValue));
+        //Atualizando a cada pressionada de tecla com o render
+        ctrl.$render();
       });
     }
   };
